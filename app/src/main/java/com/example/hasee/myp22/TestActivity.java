a8f1272b24109ee6dbdfd94c868d3eaab5d21719
dikerdl <18108195173@163.com>
2017-05-15 18:25:39 +0800

    add TestActivity class

---
+++ app/src/main/java/com/example/hasee/myp22/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.hasee.myp22;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hasee on 2017/5/15.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
