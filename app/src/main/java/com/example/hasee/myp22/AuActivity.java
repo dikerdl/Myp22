39eca4e17dee8e490fbe61587ca950ce8ea1bf60
dikerdl <18108195173@163.com>
2017-05-15 18:39:39 +0800

    commit s

---
+++ app/src/main/java/com/example/hasee/myp22/AuActivity.java
@@ -0,0 +1,16 @@
+package com.example.hasee.myp22;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hasee on 2017/5/15.
+ */
+
+public class AuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
